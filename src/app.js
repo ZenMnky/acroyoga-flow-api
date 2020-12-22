fe751082e672858de796b1d62a9e60e6ba4c8756
Justin Hager <coachjustinhager@gmail.com>
2020-12-22 15:16:25 -0800

    add winston logging modularize error-handler modularize validate-bearer-token

--- src/app.js
+++ src/app.js
@@ -3,7 +3,11 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
+
 const { NODE_ENV } = require('./config');
+const validateBearerToken = require('./validate-bearer-token');
+const errorHandler = require('./error-handler');
+
 // const someRouter = require('./some-routers/some-router');
 
 const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
@@ -20,27 +24,6 @@ app.get('/', (req, res) => {
   res.send('Hello, boilerplate!')
 });
 
-function validateBearerToken(req, res, next) {
-  const apiToken = process.env.API_TOKEN
-  const authToken = req.get('Authorization')
-  
-  if (!authToken || authToken.split(' ')[1] !== apiToken) {
-      logger.error(`Unauthorized request to path: ${req.path}`)
-      return res.status(401).json({ error: 'Unauthorized request' })
-  }
-  // move to the next middleware
-  next()
-}
-
-app.use(function errorHandler(error, req, res, next) {
-  let response;
-  if (NODE_ENV === 'production') {
-    response = { error: { message: 'server error' } };
-  } else {
-    console.error(error);
-    response = { message: error.message, error };
-  }
-  res.status(500).json(response);
-});
+app.use(errorHandler(error, req, res, next));
 
 module.exports = app;
