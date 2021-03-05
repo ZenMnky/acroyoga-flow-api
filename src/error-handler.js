6bc106c70d235b2a733dc6a58fa6ea504e17fdff
Justin Hager <coachjustinhager@gmail.com>
2021-03-05 10:03:27 -0800

    update errorHandler console logging

--- src/error-handler.js
+++ src/error-handler.js
@@ -4,12 +4,13 @@ const logger = require('./logger')
 function errorHandler(error, req, res, next) {
   let response
   if (NODE_ENV === 'production') {
+
     response = { error: { message: 'server error' } }
   } else {
-    console.error(error)
     logger.error(error.message)
     response = { message: error.message, error }
   }
+  console.error(error);
   res.status(500).json(response)
 }
 
